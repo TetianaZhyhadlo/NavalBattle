f40a9965ec8f3db26a20e20e2542940c655c533c
Татьяна Жигадло <t.zhyhadlo@gmail.com>
2020-02-20 21:35:03 +0200

    Add project files.

---
+++ Map.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalBattle
+{
+    public class Cell
+    {
+
+    }
+    public class Map
+    {
+        public int Field { get; }
+        public Cell[,] Cells { get; }
+        public Map()
+        {
+            int field = 12;
+            Field = field;
+            Cells = new Cell[field, field];
+
+        }
+        public void CreateMap()
+        {
+            for (int i = 0; i < Field; i++)
+            {
+                for (int k = 0; k < Field; k++)
+                {
+                    Cells[i, k] = new Cell();
+                }
+            }
+        }
+        public void ShowMap()
+        {
+            for (int i = 0; i < Field-1; i++)
+            {
+                Console.WriteLine();
+                for (int k = 0; k < Field-1; k++)
+                {
+                    Console.Write("\t|");
+                    if (k == 0 && i!=0)
+                        Console.Write(i);
+                    if (i == 0 && k!=0)
+                        Console.Write(k);
+
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NavalBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name for player1");
+            Player player1 = new Player();
+            player1.Name = Console.ReadLine();
+            player1.Number = 1;
+            player1.CreateMap();
+            player1.ShowMap();
+
+            
+            Console.WriteLine("Enter name for player2");
+            Player player2 = new Player();
+            player2.Name = Console.ReadLine();
+            player2.Number = 2;
+            player2.CreateMap();
+            player2.ShowMap();
+            
+
+        }
+    }
+}
